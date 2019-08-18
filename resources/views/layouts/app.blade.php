0409ad8e2d30c83c8600cbcf9aad0f629b805814
Gerard O'Neill <grardb@gmail.com>
2019-08-18 09:40:15 -0700

    Add admin link to navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,52 +22,7 @@
   </head>
   <body>
     <div id="app">
-      <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-        <div class="container">
-          {{ link_to_route('home', config('app.name', 'Laravel'), null, ['class' => 'navbar-brand']) }}
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <!-- Left Side Of Navbar -->
-            <ul class="navbar-nav mr-auto">
-
-            </ul>
-
-            <!-- Right Side Of Navbar -->
-            <ul class="navbar-nav ml-auto">
-              <!-- Authentication Links -->
-              @guest
-                <li class="nav-item">
-                  {{ link_to_route('login', __('Login'), null, ['class' => 'nav-link']) }}
-                </li>
-                @if (Route::has('register'))
-                  <li class="nav-item">
-                    {{ link_to_route('register', __('Register'), null, ['class' => 'nav-link']) }}
-                  </li>
-                @endif
-              @else
-                <li class="nav-item dropdown">
-                  <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                    {{ Auth::user()->name }} <span class="caret"></span>
-                  </a>
-
-                  <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                    {{ link_to_route('logout', __('Logout'), null, [
-                      'class' => 'dropdown-item',
-                      'onclick' => "event.preventDefault(); document.getElementById('logout-form').submit();",
-                    ]) }}
-
-                    {{ Form::open(['route' => 'logout', 'id' => 'logout-form', 'class' => 'd-none']) }}
-                    {{ Form::close() }}
-                  </div>
-                </li>
-              @endguest
-            </ul>
-          </div>
-        </div>
-      </nav>
+      @include('inc.navbar')
 
       <main class="py-4">
         @yield('content')
