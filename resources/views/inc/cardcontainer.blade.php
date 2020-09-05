6b59522b30d05eef00b42a44240dabd3dea67b72
Alex Patrick <alex@chinstraplabs.com>
2020-09-05 11:16:10 -0400

    Fix hardcoded stuff in views in case not seeded

--- resources/views/inc/cardcontainer.blade.php
+++ resources/views/inc/cardcontainer.blade.php
@@ -1,27 +1,3 @@
-<div class="d-flex flex-column">
-  <div class="mb-4">
-    <div class="w-100">
-    </div>
-    <div class="row">
-      @each('inc.card', $groups[1], 'item')
-    </div>
-  </div>
-
-  <div class="mb-4">
-    <div class="w-100">
-      <h2 class="display-4">{{ __('Recommended') }}</h2>
-    </div>
-    <div class="row">
-      @each('inc.card', $groups[2], 'item')
-    </div>
-
-  </div>
-  <div class="mb-4">
-    <div class="w-100">
-      <h2 class="display-4">{{__('Custom')}}</h2>
-    </div>
-    <div class="row">
-      @each('inc.card', $groups[3], 'item')
-    </div>
-  </div>
+<div class="row">
+  @each('inc.card', $group, 'item')
 </div>
