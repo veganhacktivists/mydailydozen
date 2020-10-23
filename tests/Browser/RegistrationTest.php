3f32720ddc806c6feaa9731c36411b02a93c9f43
Alex Patrick <alex@alexpatrick.dev>
2020-10-23 18:18:57 -0400

    Browser tests (#29)

---
+++ tests/Browser/RegistrationTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class RegistrationTest extends DuskTestCase
+{
+    
+}
