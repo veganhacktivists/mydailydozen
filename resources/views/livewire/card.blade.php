05a7f9c8e6c1a78e2e2aeba7969e68425873c0ff
Michael <itsmichaeldiego@gmail.com>
2020-11-01 02:21:38 +0100

    Improve styles and responsive

--- resources/views/livewire/card.blade.php
+++ resources/views/livewire/card.blade.php
@@ -4,14 +4,18 @@
       <div class="flex-shrink-0 p-2 rounded-lg" style="background:lightgrey;">
         <img class="w-12 h-12" src="{{ $group->icon_location }}" alt="Icon">
       </div>
-      <div class="ml-3 flex-1">
+      <div class="flex flex-col pl-3 flex-1 truncate">
         <div class="flex-1 flex items-center mb-2">
-          <h6 class="text-xl leading-5 font-bold text-dark truncate mr-3 pb-1">
-            {{ $group['name'] }}
-          </h6>
-          <a href="/groups/{{ $group['id'] }}/">
-            <img src="/assets/icon-info.svg" class="w-5 h-5" />
-          </a>
+          <div class="flex-0 min-w-0 truncate">
+            <h6 class="text-xl font-bold text-dark mr-3 pb-1 truncate">
+              {{ $group['name'] }}
+            </h6>
+          </div>
+          <div class="flex-0 flex-shrink-0">
+            <a href="/groups/{{ $group['id'] }}/">
+              <img src="/assets/icon-info.svg" class="w-6 h-6" />
+            </a>
+          </div>
         </div>
         <div class="flex items-end justify-end">
           <span class="text-muted text-xs">{{ $checkCount ?? 0 }} / {{ $group->per_day}}</span>
@@ -28,12 +32,12 @@
       </div>
     </div>
   </div>
-  <div class="flex justify-end px-3 py-2">
-    <a href="/groups/{{ $group['id'] }}/edit" class="flex items-center">
-      <span class="font-medium mr-1">Edit</span>
-      <img src="/assets/icon-pencil.svg" class="w-6 h-6 color-inherit" />
-    </a>
-  </div>
-  <!-- @if (Auth::user()->isAdmin()) -->
-  <!-- @endif -->
+  @if (Auth::user()->isAdmin())
+    <div class="flex justify-end px-3 py-2">
+      <a href="/groups/{{ $group['id'] }}/edit" class="flex items-center">
+        <span class="font-medium mr-1">Edit</span>
+        <img src="/assets/icon-pencil.svg" class="w-6 h-6" />
+      </a>
+    </div>
+  @endif
 </div>
