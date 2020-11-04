99052100614de0ab4594da5d1f8c04dda1d74b60
Pablo Kebees <pablopetje@gmail.com>
2020-11-04 19:47:12 +0100

    Update master (#49)
    * Remove empty files
    
    * Remove some boilerplate/visible comments
    
    * Send help users to contact page
    
    * Clean up Category imports
    
    * Contact Ticket comments
    
    * Serving size comments
    
    * Start cleaning up users
    
    * Start on User
    
    * Nav button didn't work
    
    * Refactor/models comments etc (#42)
    
    * Refactor group
    
    * Clean up some stuff on history page
    
    * Clean up dashboard
    
    * Set example email
    
    * Optimize some user queries, clean it up
    
    * Revert "Merge branch 'master' into develop"
    
    This reverts commit 1c2144cc9a7996c8e88a3e411d7268ac7e752fc3.
    
    * Add Sass
    
    * TASK-65: Improve Card styles (#47)
    
    * Develop (#45)
    
    Deploy
    
    * Connect contact form (#46)
    
    * connect contact form to backend
    
    * uncomment mail send
    
    * Checkbox functionality fix
    
    * Improve Cards position
    
    * Improve layout and add dummy_icon
    
    * Add cursor: pointer for better accesibility
    
    * Avoid unnecessary classes
    
    * Add custom border when fulfilled
    
    * Improve admin view
    
    * Improve styles and responsive
    
    * Improve checkbox colors
    
    Co-authored-by: danielm2048 <danielmimoun98@gmail.com>
    
    * Improve welcome pagestyles (#48)
    
    Co-authored-by: Alex Patrick <alex@alexpatrick.dev>
    Co-authored-by: Michael Diego <itsmichaeldiego@gmail.com>
    Co-authored-by: danielm2048 <danielmimoun98@gmail.com>

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,11 +1,11 @@
 <footer>
     <div class="bg-white">
-        <div class="max-w-screen-xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
-            <div class="xl:grid xl:grid-cols-3 xl:gap-8">
-                 <p class="mt-8 text-base leading-6 text-gray-400 md:mt-0 md:order-1">
+        <div class="max-w-screen-xl mx-auto py-4 px-4 sm:px-6 lg:px-8">
+            <div class="flex flex-wrap items-center justify-between">
+                 <p class="text-base leading-6 text-gray-400 md:mt-0 md:order-1">
                     Built with love by the Vegan Hacktivists 💕
                 </p>
-                <p class="mt-8 text-base leading-6 text-gray-400 md:mt-0 md:order-1">
+                <p class="text-base leading-6 text-gray-400 md:mt-0 md:order-1">
                     <a href="https://play.google.com/store/apps/details?id=org.nutritionfacts.dailydozen&hl=en" target="_blank">Android</a>
                     <a href="https://apps.apple.com/us/app/dr-gregers-daily-dozen/id1060700802" target="_blank">iOS</a>
                     This app is <strong>not</strong> officially supported by <a href="https://nutritionfacts.org" target="_blank">Dr. Michael Greger</a>
--- resources/views/livewire/card.blade.php
+++ resources/views/livewire/card.blade.php
@@ -1,43 +1,43 @@
-<div class="bg-white overflow-hidden shadow rounded-lg">
-  <div class="p-5">
+<div class="{{ $checkCount ===  $group->per_day ? 'border-pine-400' : ''  }} border-2 bg-white overflow-hidden shadow rounded-lg">
+  <div class="p-4">
     <div class="flex items-center">
-      <div class="flex-shrink-0">
-        <img class="w-6 h-6 text-cool-gray-400" src="{{ $group->icon_location }}" alt="Icon">
+      <div class="flex-shrink-0 p-2 rounded-lg" style="background:lightgrey;">
+        <img class="w-12 h-12" src="{{ $group->icon_location }}" alt="Icon">
       </div>
-      <div class="ml-5 w-0 flex-1">
-        <dl>
-          <dt class="text-sm leading-5 font-medium text-cool-gray-500 truncate">
-            {{ $group['name'] }}
-            <span class="text-muted text-xs">{{ $checkCount ?? 0 }} / {{ $group->per_day}}</span>
-
-          </dt>
-          <dd>
-            <div class="text-lg leading-7 font-medium text-cool-gray-900">
-              @for ($i = 0; $i < $group['per_day']; $i++) @if($i < $checkCount) <input type="checkbox"
-                class="customCheckbox" id="{{ $group['name'].$i }}" wire:click.prevent="uncheck" checked>
+      <div class="flex flex-col pl-3 flex-1 truncate">
+        <div class="flex-1 flex items-center mb-2">
+          <div class="flex-0 min-w-0 truncate">
+            <h6 class="text-xl font-bold text-dark mr-3 pb-1 truncate">
+              {{ $group['name'] }}
+            </h6>
+          </div>
+          <div class="flex-0 flex-shrink-0">
+            <a href="/groups/{{ $group['id'] }}/">
+              <img src="/assets/icon-info.svg" class="w-6 h-6" />
+            </a>
+          </div>
+        </div>
+        <div class="flex items-end justify-end">
+          <span class="text-muted text-xs">{{ $checkCount ?? 0 }} / {{ $group->per_day}}</span>
+          <div class="flex text-lg leading-7 font-medium text-cool-gray-900">
+            @for ($i = 0; $i < $group['per_day']; $i++) 
+              @if($i < $checkCount) 
+                <input type="checkbox" class="form-checkbox w-6 h-6 ml-2 text-pine-600" style="cursor: pointer;" id="{{ $group['name'].$i }}" wire:click.prevent="uncheck" checked>
                 @else
-                <input type="checkbox" class="customCheckbox" id="{{ $group['name'].$i }}" wire:click.prevent="check">
-                @endif
-
-                @endfor
-            </div>
-          </dd>
-        </dl>
+                <input type="checkbox" class="form-checkbox w-6 h-6 ml-2 text-pine-600" style="cursor: pointer;" id="{{ $group['name'].$i }}" wire:click.prevent="check">
+              @endif
+            @endfor
+          </div>
+        </div>
       </div>
     </div>
   </div>
-  <div class="bg-cool-gray-50 px-5 py-3">
-    <div class="text-sm leading-5">
-      <a href="/groups/{{ $group['id'] }}/"
-        class="font-medium text-teal-600 hover:text-teal-900 transition ease-in-out duration-150">
-        Details
+  @if (Auth::user()->isAdmin())
+    <div class="flex justify-end px-3 py-2">
+      <a href="/groups/{{ $group['id'] }}/edit" class="flex items-center">
+        <span class="font-medium mr-1">Edit</span>
+        <img src="/assets/icon-pencil.svg" class="w-6 h-6" />
       </a>
-      @if (Auth::user()->isAdmin())
-      <a href="/groups/{{ $group['id'] }}/edit"
-        class="pl-5 font-medium text-teal-600 hover:text-teal-900 transition ease-in-out duration-150">
-        Edit
-      </a>
-      @endif
     </div>
-  </div>
+  @endif
 </div>
