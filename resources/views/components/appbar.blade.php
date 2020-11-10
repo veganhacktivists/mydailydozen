1eb87afa76646ba05c7279318ac1672238b7b386
Pablo Kebees <pablopetje@gmail.com>
2020-11-10 16:19:09 +0100

    Close dropdown on clickaway

--- resources/views/components/appbar.blade.php
+++ resources/views/components/appbar.blade.php
@@ -1,78 +1,79 @@
 <div class="relative z-10 flex-shrink-0 flex h-16 bg-white border-b border-gray-200 lg:border-none">
-  <button @click="mobileNavOpened = true"
-    class="px-4 border-r border-cool-gray-200 text-cool-gray-400 focus:outline-none focus:bg-cool-gray-100 focus:text-cool-gray-600 lg:hidden"
-    aria-label="Open sidebar">
-    <!-- Heroicon name: menu-alt-1 -->
-    <svg class="h-6 w-6 transition ease-in-out duration-150" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" />
-    </svg>
-  </button>
-  <!-- Search bar -->
-  <div class="flex-1 px-4 flex justify-between sm:px-6 lg:max-w-6xl lg:mx-auto lg:px-8">
-    <div class="flex-1 flex">
-      <form class="w-full flex md:ml-0" action="#" method="GET">
-        <label for="search_field" class="sr-only">Search</label>
-        <div class="relative w-full text-cool-gray-400 focus-within:text-cool-gray-600">
-          <div class="absolute inset-y-0 left-0 flex items-center pointer-events-none">
-            <svg class="h-5 w-5" fill="currentColor" viewBox="0 0 20 20">
-              <path fill-rule="evenodd" clip-rule="evenodd"
-                d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" />
-            </svg>
-          </div>
-          <input id="search_field"
-            class="block w-full h-full pl-8 pr-3 py-2 rounded-md text-cool-gray-900 placeholder-cool-gray-500 focus:outline-none focus:placeholder-cool-gray-400 sm:text-sm"
-            placeholder="Search" type="search">
-        </div>
-      </form>
-    </div>
-    <div class="ml-4 flex items-center md:ml-6">
-      <button
-        class="p-1 text-cool-gray-400 rounded-full hover:bg-cool-gray-100 hover:text-cool-gray-500 focus:outline-none focus:shadow-outline focus:text-cool-gray-500"
-        aria-label="Notifications">
-        <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-            d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
+    <button @click="mobileNavOpened = true"
+        class="px-4 border-r border-cool-gray-200 text-cool-gray-400 focus:outline-none focus:bg-cool-gray-100 focus:text-cool-gray-600 lg:hidden"
+        aria-label="Open sidebar">
+        <!-- Heroicon name: menu-alt-1 -->
+        <svg class="h-6 w-6 transition ease-in-out duration-150" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" />
         </svg>
-      </button>
-
-      <!-- Profile dropdown -->
-      <div class="ml-3 relative" x-data="{ closed: true }" @click="closed = !closed">
-        <div>
-          <button
-            class="max-w-xs flex items-center text-sm rounded-full focus:outline-none focus:bg-cool-gray-100 lg:p-2 lg:rounded-md lg:hover:bg-cool-gray-100"
-            id="user-menu" aria-label="User menu" aria-haspopup="true">
-            <img class="h-8 w-8 rounded-full" src="{{ auth()->user()->profile_photo_url }}"
-              alt="{{ auth()->user()->name }}">
-            <p class="hidden ml-3 text-cool-gray-700 text-sm leading-5 font-medium lg:block">{{ auth()->user()->name }}
-            </p>
-            <!-- Heroicon name: chevron-down -->
-            <svg class="hidden flex-shrink-0 ml-1 h-5 w-5 text-cool-gray-400 lg:block" viewBox="0 0 20 20"
-              fill="currentColor">
-              <path fill-rule="evenodd"
-                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                clip-rule="evenodd" />
-            </svg>
-          </button>
-        </div>
-        <div class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg">
-          <div :class="{ 'hidden': closed }" class="py-1 rounded-md bg-white shadow-xs" role="menu"
-            aria-orientation="vertical" aria-labelledby="user-menu">
-            <a href="/user/profile"
-              class="block px-4 py-2 text-sm text-cool-gray-700 hover:bg-cool-gray-100 transition ease-in-out duration-150"
-              role="menuitem">Your Profile</a>
-            <a href="/user/api-tokens"
-              class="block px-4 py-2 text-sm text-cool-gray-700 hover:bg-cool-gray-100 transition ease-in-out duration-150"
-              role="menuitem">API Tokens</a>
-            <form method="POST" action="{{ route('logout') }}">
-              @csrf
-              <a href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                      this.closest('form').submit();"
-                class="block px-4 py-2 text-sm text-cool-gray-700 hover:bg-cool-gray-100 transition ease-in-out duration-150"
-                role="menuitem">{{ __('Logout') }}</a>
+    </button>
+    <!-- Search bar -->
+    <div class="flex-1 px-4 flex justify-between sm:px-6 lg:max-w-6xl lg:mx-auto lg:px-8">
+        <div class="flex-1 flex">
+            <form class="w-full flex md:ml-0" action="#" method="GET">
+                <label for="search_field" class="sr-only">Search</label>
+                <div class="relative w-full text-cool-gray-400 focus-within:text-cool-gray-600">
+                    <div class="absolute inset-y-0 left-0 flex items-center pointer-events-none">
+                        <svg class="h-5 w-5" fill="currentColor" viewBox="0 0 20 20">
+                            <path fill-rule="evenodd" clip-rule="evenodd"
+                                d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" />
+                        </svg>
+                    </div>
+                    <input id="search_field"
+                        class="block w-full h-full pl-8 pr-3 py-2 rounded-md text-cool-gray-900 placeholder-cool-gray-500 focus:outline-none focus:placeholder-cool-gray-400 sm:text-sm"
+                        placeholder="Search" type="search">
+                </div>
             </form>
-          </div>
         </div>
-      </div>
+        <div class="ml-4 flex items-center md:ml-6">
+            <button
+                class="p-1 text-cool-gray-400 rounded-full hover:bg-cool-gray-100 hover:text-cool-gray-500 focus:outline-none focus:shadow-outline focus:text-cool-gray-500"
+                aria-label="Notifications">
+                <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
+                </svg>
+            </button>
+
+            <!-- Profile dropdown -->
+            <div class="ml-3 relative" x-data="{ closed: true }" @click="closed = !closed">
+                <div>
+                    <button
+                        class="max-w-xs flex items-center text-sm rounded-full focus:outline-none focus:bg-cool-gray-100 lg:p-2 lg:rounded-md lg:hover:bg-cool-gray-100"
+                        id="user-menu" aria-label="User menu" aria-haspopup="true">
+                        <img class="h-8 w-8 rounded-full" src="{{ auth()->user()->profile_photo_url }}"
+                            alt="{{ auth()->user()->name }}">
+                        <p class="hidden ml-3 text-cool-gray-700 text-sm leading-5 font-medium lg:block">
+                            {{ auth()->user()->name }}
+                        </p>
+                        <!-- Heroicon name: chevron-down -->
+                        <svg class="hidden flex-shrink-0 ml-1 h-5 w-5 text-cool-gray-400 lg:block" viewBox="0 0 20 20"
+                            fill="currentColor">
+                            <path fill-rule="evenodd"
+                                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                clip-rule="evenodd" />
+                        </svg>
+                    </button>
+                </div>
+                <div class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg">
+                    <div x-show="!closed" @click.away="closed = true" class="py-1 rounded-md bg-white shadow-xs"
+                        role="menu" aria-orientation="vertical" aria-labelledby="user-menu">
+                        <a href="/user/profile"
+                            class="block px-4 py-2 text-sm text-cool-gray-700 hover:bg-cool-gray-100 transition ease-in-out duration-150"
+                            role="menuitem">Your Profile</a>
+                        <a href="/user/api-tokens"
+                            class="block px-4 py-2 text-sm text-cool-gray-700 hover:bg-cool-gray-100 transition ease-in-out duration-150"
+                            role="menuitem">API Tokens</a>
+                        <form method="POST" action="{{ route('logout') }}">
+                            @csrf
+                            <a href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                      this.closest('form').submit();"
+                                class="block px-4 py-2 text-sm text-cool-gray-700 hover:bg-cool-gray-100 transition ease-in-out duration-150"
+                                role="menuitem">{{ __('Logout') }}</a>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
 </div>
