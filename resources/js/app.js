432a3f2a9ba20b0a2da5caef8425fa254988402e
danielm2048 <danielmimoun98@gmail.com>
2020-11-12 18:41:19 +0200

    Deleted app.js alert function

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,17 +1,2 @@
 require('./bootstrap');
 require('alpinejs');
-
-
-const closeAlertButton = document.getElementById("closeAlertButton");
-
-function closeAlert(e) {
-    let element = e.target;
-    while(element.nodeName !== "BUTTON"){
-        element = element.parentNode;
-    }
-    element.parentNode.parentNode.removeChild(element.parentNode);
-}
-
-if (closeAlertButton) {
-    closeAlertButton.addEventListener("click", closeAlert);
-}
