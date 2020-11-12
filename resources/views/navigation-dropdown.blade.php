39ddf1d0754b7dd4f8000203612e53c947c5421f
Michael Diego <itsmichaeldiego@gmail.com>
2020-11-12 03:58:21 +0000

    Polish styles (#51)
    * Improve image displayment
    
    * Improve dummy_icon for testing purposes
    
    * Remove unnecessary color
    
    * Update dummy_icon to prod one
    
    * Increase border-radius
    
    * I prefer berries icons
    
    * Improve admin mode
    
    * Fix Settins header

--- resources/views/navigation-dropdown.blade.php
+++ resources/views/navigation-dropdown.blade.php
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="flex-shrink-0 flex items-center">
                     <a href="/">
-                        <x-jet-application-mark class="block h-9 w-auto" />
+                        <x-jet-application-mark />
                     </a>
                 </div>
 
