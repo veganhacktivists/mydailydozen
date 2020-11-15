8911dfc961bd92db465b8907165c9c6f531ffefb
David van Beveren <admin@veganhacktivists.org>
2020-11-15 19:27:52 +0800

    Update history.blade.php

--- resources/views/history.blade.php
+++ resources/views/history.blade.php
@@ -21,15 +21,12 @@
   <div class="mt-8">
     <div class="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
       <x-history-calendar :history="$history"></x-history-calendar>
-	        <h2 class="text-lg leading-6 font-medium text-cool-gray-900 mb-4">Your daily maximum is: {{$totalPerDay}}</h2>
-			<br>
-		<h2 class="text-lg leading-6 font-medium text-cool-gray-900 mb-6">Head over to your <a
-          href="{{route('settings')}}" class="text-blue-500 hover:underline">settings page</a> to add or remove groups!
-		</h2>
-      <br>
+		<h2 class="text-lg leading-6 font-medium text-cool-gray-900 mb-4">Your current goal is: {{$totalPerDay}} groups a day.</h2>
+		<br>
+		<h2 class="text-lg leading-6 font-medium text-cool-gray-900 mb-6">Head over to your <a href="{{route('settings')}}" class="text-blue-500 hover:underline">settings page</a> to add or remove groups!</h2>
       <p class="mb-2 bg-green-100 text-green-600 inline-block p-3 border border-green-600 rounded" style="background-color:#9cd99c80;color: #086c10;">Green is greater than 60%</p>
-		<p class="mb-2 bg-yellow-100 text-yellow-600 inline-block p-3 border border-yellow-600 rounded" style="color: #7d7119;">Yellow is between 30 and 60%.</p>
-		<p class="mb-2 bg-gray-100 text-gray-600 inline-block p-3 border border-gray-600 rounded">Gray is 0%</p>
+		<p class="mb-2 bg-yellow-100 text-yellow-600 inline-block p-3 border border-yellow-600 rounded" style="margin-left: 10px;color: #7d7119;">Yellow is between 30 and 60%.</p>
+		<p class="mb-2 bg-gray-100 text-gray-600 inline-block p-3 border border-gray-600 rounded" style="margin-left: 10px;background-color: #e8e8e8;color: #757575;">Gray is 0%</p>
     </div>
     <pre>
     </pre>
