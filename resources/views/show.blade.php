3ca89d97693ff7d003e6cdf71f3ac27cb9e65c86
Pablo Kebees <pablopetje@gmail.com>
2020-11-15 12:27:34 +0100

    Fix ridiculous banner size

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,51 +1,58 @@
 <x-master>
-  <!-- Page header -->
-  <div class="bg-white shadow">
-    <div class="px-4 sm:px-6 lg:max-w-6xl lg:mx-auto lg:px-8">
-      <div class="py-6 md:flex md:items-center md:justify-between lg:border-t lg:border-cool-gray-200">
-        <div class="flex-1 min-w-0">
-          <div class="flex items-center">
-            <div>
-              <div class="flex items-center">
-                <h1 class="ml-3 text-2xl font-bold leading-7 text-cool-gray-900 sm:leading-9 sm:truncate">
-                  Edit Group
-                </h1>
-              </div>
+    <!-- Page header -->
+    <div class="bg-white shadow">
+        <div class="px-4 sm:px-6 lg:max-w-6xl lg:mx-auto lg:px-8">
+            <div class="py-6 md:flex md:items-center md:justify-between lg:border-t lg:border-cool-gray-200">
+                <div class="flex-1 min-w-0">
+                    <div class="flex items-center">
+                        <div>
+                            <div class="flex items-center">
+                                <h1
+                                    class="ml-3 text-2xl font-bold leading-7 text-cool-gray-900 sm:leading-9 sm:truncate">
+                                    Edit Group
+                                </h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="mt-6 flex space-x-3 md:mt-0 md:ml-4">
+                    <span class="shadow-sm rounded-md">
+                    </span>
+                    <span class="shadow-sm rounded-md">
+                    </span>
+                </div>
             </div>
-          </div>
         </div>
-        <div class="mt-6 flex space-x-3 md:mt-0 md:ml-4">
-          <span class="shadow-sm rounded-md">
-          </span>
-          <span class="shadow-sm rounded-md">
-          </span>
-        </div>
-      </div>
     </div>
-  </div>
 
     <div class="relative py-16 bg-white overflow-hidden">
         <div class="hidden lg:block lg:absolute lg:inset-y-0 lg:h-full lg:w-full">
             <div class="relative h-full text-lg max-w-prose mx-auto">
-                <svg class="absolute top-12 left-full transform translate-x-32" width="404" height="384" fill="none" viewBox="0 0 404 384">
+                <svg class="absolute top-12 left-full transform translate-x-32" width="404" height="384" fill="none"
+                    viewBox="0 0 404 384">
                     <defs>
-                        <pattern id="74b3fd99-0a6f-4271-bef2-e80eeafdf357" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
+                        <pattern id="74b3fd99-0a6f-4271-bef2-e80eeafdf357" x="0" y="0" width="20" height="20"
+                            patternUnits="userSpaceOnUse">
                             <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
                         </pattern>
                     </defs>
                     <rect width="404" height="384" fill="url(#74b3fd99-0a6f-4271-bef2-e80eeafdf357)" />
                 </svg>
-                <svg class="absolute top-1/2 right-full transform -translate-y-1/2 -translate-x-32" width="404" height="384" fill="none" viewBox="0 0 404 384">
+                <svg class="absolute top-1/2 right-full transform -translate-y-1/2 -translate-x-32" width="404"
+                    height="384" fill="none" viewBox="0 0 404 384">
                     <defs>
-                        <pattern id="f210dbf6-a58d-4871-961e-36d5016a0f49" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
+                        <pattern id="f210dbf6-a58d-4871-961e-36d5016a0f49" x="0" y="0" width="20" height="20"
+                            patternUnits="userSpaceOnUse">
                             <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
                         </pattern>
                     </defs>
                     <rect width="404" height="384" fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)" />
                 </svg>
-                <svg class="absolute bottom-12 left-full transform translate-x-32" width="404" height="384" fill="none" viewBox="0 0 404 384">
+                <svg class="absolute bottom-12 left-full transform translate-x-32" width="404" height="384" fill="none"
+                    viewBox="0 0 404 384">
                     <defs>
-                        <pattern id="d3eb07ae-5182-43e6-857d-35c643af9034" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
+                        <pattern id="d3eb07ae-5182-43e6-857d-35c643af9034" x="0" y="0" width="20" height="20"
+                            patternUnits="userSpaceOnUse">
                             <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
                         </pattern>
                     </defs>
@@ -55,37 +62,43 @@
         </div>
         <div class="relative px-4 sm:px-6 lg:px-8">
             <div class="text-lg max-w-prose mx-auto mb-6">
-                <p class="text-base text-center leading-6 text-teal-600 font-semibold tracking-wide uppercase">More About</p>
-                <h1 class="mt-2 mb-8 text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10">{{ $group->name }}</h1>
+                <p class="text-base text-center leading-6 text-teal-600 font-semibold tracking-wide uppercase">More
+                    About</p>
+                <h1
+                    class="mt-2 mb-8 text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10">
+                    {{ $group->name }}</h1>
             </div>
             <div x-data="{ metric: true }" class="prose prose-lg text-gray-500 mx-auto">
                 <figure>
-                    <img class="w-full rounded-lg" src="{{ asset($group->banner_location) }}" alt="{{ $group->name }}" width="1310" height="873">
+                    <img class="w-full h-32 rounded-lg" src="{{ asset($group->banner_location) }}"
+                        alt="{{ $group->name }} header">
                     <figcaption>{{ $group->name }}</figcaption>
                 </figure>
                 <h3>{{ __('Serving Sizes') }}</h3>
                 <div class="btn-group btn-group-toggle inline" data-toggle="buttons">
                     <span class="inline-flex rounded-md shadow-sm">
-                      <button x-on:click="metric = false" class="inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs leading-4 font-medium rounded text-white bg-teal-600 hover:bg-teal-500 focus:outline-none focus:border-teal-700 focus:shadow-outline-teal active:bg-teal-700 transition ease-in-out duration-150">
-                        Imperial
-                      </button>
+                        <button x-on:click="metric = false"
+                            class="inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs leading-4 font-medium rounded text-white bg-teal-600 hover:bg-teal-500 focus:outline-none focus:border-teal-700 focus:shadow-outline-teal active:bg-teal-700 transition ease-in-out duration-150">
+                            Imperial
+                        </button>
                     </span>
                     <span class="inline-flex rounded-md shadow-sm">
-                      <button x-on:click="metric = true" class="inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs leading-4 font-medium rounded text-white bg-teal-600 hover:bg-teal-500 focus:outline-none focus:border-teal-700 focus:shadow-outline-teal active:bg-teal-700 transition ease-in-out duration-150">
-                        Metric
-                      </button>
+                        <button x-on:click="metric = true"
+                            class="inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs leading-4 font-medium rounded text-white bg-teal-600 hover:bg-teal-500 focus:outline-none focus:border-teal-700 focus:shadow-outline-teal active:bg-teal-700 transition ease-in-out duration-150">
+                            Metric
+                        </button>
                     </span>
                 </div>
                 @foreach ($servingSizes as $servingSize)
-                    <p x-show="!metric">{{ $servingSize->size_imperial }}</p>
-                    <p x-show="metric">{{ $servingSize->size_metric }}</p>
+                <p x-show="!metric">{{ $servingSize->size_imperial }}</p>
+                <p x-show="metric">{{ $servingSize->size_metric }}</p>
                 @endforeach
                 <h3>{{ __('Detail Types') }}</h3>
                 @foreach ($detailTypes as $detailType)
-                    <p>{{ $detailType->video }}</p>
+                <p>{{ $detailType->video }}</p>
                 @endforeach
             </div>
         </div>
     </div>
-  </div>
+    </div>
 </x-master>
