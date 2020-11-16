bdeecacb29f22e9511caac23ef7586236982f498
Michael <itsmichaeldiego@gmail.com>
2020-11-16 23:56:01 +0100

    Improve card-toggle and card

--- resources/views/livewire/card-toggle.blade.php
+++ resources/views/livewire/card-toggle.blade.php
@@ -1,27 +1,36 @@
-<div class="bg-white overflow-hidden shadow rounded-lg">
-  <div class="p-5 cursor-pointer" wire:click="toggleGroup">
-    <div class="flex justify-between">
-      <div class="flex items-center">
-        <dl>
-          <dt class="text-sm leading-5 font-medium text-cool-gray-500 truncate">
-            {{ $group['name'] }}
-          </dt>
-        </dl>
-      </div>
-      <div class="text-pine-500">
-        @if($checked)
-        <x-icons.check-circle />
-
-        @endif
-      </div>
-
+<div
+  wire:click="toggleGroup"
+  class="{{ $checked ? 'border-pine-400' : ''  }} p-4 border-2 bg-white overflow-hidden rounded-2xl cursor-pointer"
+>
+    <div class="flex">
+        <div class="flex-shrink-0 rounded-xl overflow-hidden">
+            <img class="w-16 h-16" src="{{ $group->icon_location }}" alt="Icon">
+        </div>
+        <div class="flex flex-col pl-3 flex-1">
+            <div class="flex items-center justify-between">
+                <div class="flex-1 flex items-center truncate">
+                    <div class="flex-0 min-w-0 truncate">
+                        <h6 class="text-xl font-bold text-dark mr-3 pb-1 truncate">
+                            {{ $group['name'] }}
+                        </h6>
+                    </div>
+                    <div class="flex flex-0 flex-shrink-0">
+                        <a href="/groups/{{ $group['id'] }}/">
+                            <img src="/assets/icon-info.svg" class="w-6 h-6" />
+                        </a>
+                    </div>
+                </div>
+                @if (Auth::user()->isAdmin())
+                <a href="/groups/{{ $group['id'] }}/edit" class="ml-1">
+                    <img src="/assets/icon-pencil.svg" class="w-6 h-6" />
+                </a>
+                @endif
+            </div>
+            <div class="flex-1 flex items-end justify-end text-pine-500">
+              @if ($checked)
+                <x-icons.check-circle />
+              @endif
+            </div>
+        </div>
     </div>
-  </div>
-  <div class="bg-cool-gray-50 px-5 py-3">
-    <div class="text-sm leading-5">
-      <a href="#" class="font-medium text-teal-600 hover:text-teal-900 transition ease-in-out duration-150">
-        Details
-      </a>
-    </div>
-  </div>
 </div>
