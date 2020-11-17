d3e1929e26e0424bd82270448526ae4201dd6d1d
Michael Diego <itsmichaeldiego@gmail.com>
2020-11-17 02:22:22 +0100

    Improve settings / groups page (#54)
    * Remove heart icon
    
    * Improve card-toggle and card
    
    * Improve settings styles

--- resources/views/livewire/card-toggle.blade.php
+++ resources/views/livewire/card-toggle.blade.php
@@ -1,38 +1,36 @@
-<div class="bg-white overflow-hidden shadow rounded-lg">
-  <div class="p-5 cursor-pointer" wire:click="toggleGroup">
-    <div class="flex justify-between">
-      <div class="flex items-center">
-        <div class="flex-shrink-0">
-          <svg class="w-6 h-6 text-cool-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-              d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z">
-            </path>
-          </svg>
+<button
+  wire:click="toggleGroup"
+  class="{{ $checked ? 'border-pine-400' : ''  }} p-4 border-2 bg-white overflow-hidden rounded-2xl focus:outline-none focus:shadow-outline"
+>
+    <div class="flex">
+        <div class="flex-shrink-0 rounded-xl overflow-hidden">
+            <img class="w-16 h-16" src="{{ $group->icon_location }}" alt="Icon">
         </div>
-        <div class="ml-5 flex-1">
-          <dl>
-            <dt class="text-sm leading-5 font-medium text-cool-gray-500 truncate">
-              {{ $group['name'] }}
-            </dt>
-
-          </dl>
+        <div class="flex flex-col pl-3 flex-1">
+            <div class="flex items-center justify-between">
+                <div class="flex-1 flex items-center truncate">
+                    <div class="flex-0 min-w-0 truncate">
+                        <h6 class="text-xl font-bold text-dark mr-3 pb-1 truncate">
+                            {{ $group['name'] }}
+                        </h6>
+                    </div>
+                    <div class="flex flex-0 flex-shrink-0">
+                        <a href="/groups/{{ $group['id'] }}/">
+                            <img src="/assets/icon-info.svg" class="w-6 h-6" />
+                        </a>
+                    </div>
+                </div>
+                @if (Auth::user()->isAdmin())
+                <a href="/groups/{{ $group['id'] }}/edit" class="ml-1">
+                    <img src="/assets/icon-pencil.svg" class="w-6 h-6" />
+                </a>
+                @endif
+            </div>
+            <div class="flex-1 flex items-end justify-end text-pine-500">
+              @if ($checked)
+                <x-icons.check-circle />
+              @endif
+            </div>
         </div>
-      </div>
-      <div class="text-pine-500">
-        @if($checked)
-        <x-icons.check-circle />
-
-        @endif
-      </div>
-
     </div>
-  </div>
-  <div class="bg-cool-gray-50 px-5 py-3">
-    <div class="text-sm leading-5">
-      <a href="#" class="font-medium text-teal-600 hover:text-teal-900 transition ease-in-out duration-150">
-        Details
-      </a>
-    </div>
-  </div>
-</div>
+</button>
--- resources/views/livewire/card.blade.php
+++ resources/views/livewire/card.blade.php
@@ -1,43 +1,40 @@
-<div
-    class="{{ $checkCount ===  $group->per_day ? 'border-pine-400' : ''  }} border-2 bg-white overflow-hidden rounded-2xl">
-    <div class="p-4">
-        <div class="flex items-center">
-            <div class="flex-shrink-0 rounded-xl overflow-hidden">
-                <img class="w-16 h-16" src="{{ $group->icon_location }}" alt="Icon">
-            </div>
-            <div class="flex flex-col pl-3 flex-1">
-                <div class="flex items-center justify-between mb-2">
-                    <div class="flex-1 flex items-center truncate">
-                        <div class="flex-0 min-w-0 truncate">
-                            <h6 class="text-xl font-bold text-dark mr-3 pb-1 truncate">
-                                {{ $group['name'] }}
-                            </h6>
-                        </div>
-                        <div class="flex flex-0 flex-shrink-0">
-                            <a href="/groups/{{ $group['id'] }}/">
-                                <img src="/assets/icon-info.svg" class="w-6 h-6" />
-                            </a>
-                        </div>
+<div class="{{ $checkCount ===  $group->per_day ? 'border-pine-400' : ''  }} border-2 p-4 bg-white overflow-hidden rounded-2xl">
+    <div class="flex">
+        <div class="flex-shrink-0 rounded-xl overflow-hidden">
+            <img class="w-16 h-16" src="{{ $group->icon_location }}" alt="Icon">
+        </div>
+        <div class="flex flex-col pl-3 flex-1">
+            <div class="flex items-center justify-between">
+                <div class="flex-1 flex items-center truncate">
+                    <div class="flex-0 min-w-0 truncate">
+                        <h6 class="text-xl font-bold text-dark mr-3 pb-1 truncate">
+                            {{ $group['name'] }}
+                        </h6>
                     </div>
-                    @if (Auth::user()->isAdmin())
-                    <a href="/groups/{{ $group['id'] }}/edit" class="ml-1">
-                        <img src="/assets/icon-pencil.svg" class="w-6 h-6" />
-                    </a>
-                    @endif
+                    <div class="flex flex-0 flex-shrink-0">
+                        <a href="/groups/{{ $group['id'] }}/">
+                            <img src="/assets/icon-info.svg" class="w-6 h-6" />
+                        </a>
+                    </div>
                 </div>
-                <div class="flex items-end justify-end">
-                    <div class="flex items-center">
-                        <span class="text-muted text-xs">{{ $checkCount ?? 0 }} / {{ $group->per_day}}</span>
-                        <div class="flex text-lg leading-7 font-medium text-cool-gray-900">
-                            @for ($i = 0; $i < $group['per_day']; $i++) @if($i < $checkCount) <input type="checkbox"
-                                class="form-checkbox w-6 h-6 ml-2 text-pine-600" style="cursor: pointer;"
-                                id="{{ $group['name'].$i }}" wire:click.prevent="uncheck" checked>
-                                @else
-                                <input type="checkbox" class="form-checkbox w-6 h-6 ml-2 text-pine-600"
-                                    style="cursor: pointer;" id="{{ $group['name'].$i }}" wire:click.prevent="check">
-                                @endif
-                                @endfor
-                        </div>
+                @if (Auth::user()->isAdmin())
+                <a href="/groups/{{ $group['id'] }}/edit" class="ml-1">
+                    <img src="/assets/icon-pencil.svg" class="w-6 h-6" />
+                </a>
+                @endif
+            </div>
+            <div class="flex-1 flex items-end justify-end">
+                <div class="flex items-center">
+                    <span class="text-muted text-xs">{{ $checkCount ?? 0 }} / {{ $group->per_day}}</span>
+                    <div class="flex text-lg leading-7 font-medium text-cool-gray-900">
+                        @for ($i = 0; $i < $group['per_day']; $i++) @if($i < $checkCount) <input type="checkbox"
+                            class="form-checkbox w-6 h-6 ml-2 text-pine-600" style="cursor: pointer;"
+                            id="{{ $group['name'].$i }}" wire:click.prevent="uncheck" checked>
+                            @else
+                            <input type="checkbox" class="form-checkbox w-6 h-6 ml-2 text-pine-600"
+                                style="cursor: pointer;" id="{{ $group['name'].$i }}" wire:click.prevent="check">
+                            @endif
+                            @endfor
                     </div>
                 </div>
             </div>
