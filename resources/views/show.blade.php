97d5845360e3e8cfa82e0ceda598e4cb82870a75
David van Beveren <admin@veganhacktivists.org>
2020-11-17 11:08:34 +0800

    Update show.blade.php

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -3,18 +3,6 @@
     <div class="bg-white shadow">
         <div class="px-4 sm:px-6 lg:max-w-6xl lg:mx-auto lg:px-8">
             <div class="py-6 md:flex md:items-center md:justify-between lg:border-t lg:border-cool-gray-200">
-                <div class="flex-1 min-w-0">
-                    <div class="flex items-center">
-                        <div>
-                            <div class="flex items-center">
-                                <h1
-                                    class="ml-3 text-2xl font-bold leading-7 text-cool-gray-900 sm:leading-9 sm:truncate">
-                                    Edit Group
-                                </h1>
-                            </div>
-                        </div>
-                    </div>
-                </div>
                 <div class="mt-6 flex space-x-3 md:mt-0 md:ml-4">
                     <span class="shadow-sm rounded-md">
                     </span>
