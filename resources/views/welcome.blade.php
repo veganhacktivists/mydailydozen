aa0b444283527b8a83da9b5ed749e82cf9032e44
David van Beveren <admin@veganhacktivists.org>
2020-11-25 15:12:03 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,14 +19,13 @@
                                         <img class="h-8 w-auto sm:h-10" src="{{ asset('img/mddlogo.png') }}" alt="Logo">
                                     </a>
                                     <div class="-mr-2 flex items-center md:hidden">
-                                        <button type="button"
-                                                class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
-                                                id="main-menu" aria-label="Main menu" aria-haspopup="true">
-                                            <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                                    d="M4 6h16M4 12h16M4 18h16"/>
-                                            </svg>
-                                        </button>
+                                <a href="/contact"
+                                class="font-medium text-gray-500 hover:text-gray-900 transition duration-150 ease-in-out">Contact</a>
+                                <a href="{{ route('login') }}"
+                                class="ml-8 font-medium text-pine-600 hover:text-pine-900 transition duration-150 ease-in-out">Log
+                                    in</a>
+                                <a href="{{ route('register') }}"
+                                class="ml-8 font-medium text-pine-600 hover:text-pine-900 transition duration-150 ease-in-out">Register</a>
                                     </div>
                                 </div>
                             </div>
