f200a872b917605a8d6ae5ad354773aea0c74df8
David van Beveren <admin@veganhacktivists.org>
2020-11-25 15:33:49 +0800

    nav bar menu for contact page

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -21,39 +21,35 @@
 
             <div class="relative pt-6 px-4 sm:px-6 lg:px-8">
                 <nav class="relative flex items-center justify-between sm:h-10 lg:justify-start">
-                    <div class="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
-                        <div class="flex items-center justify-between w-full md:w-auto">
-                            <a href="#" aria-label="Home">
-                                <img class="h-8 w-auto sm:h-10" src="{{ asset('img/mddlogo.png') }}" alt="Logo">
-                            </a>
-                            <div class="-mr-2 flex items-center md:hidden">
-                                <button type="button"
-                                        class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
-                                        id="main-menu" aria-label="Main menu" aria-haspopup="true">
-                                    <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                              d="M4 6h16M4 12h16M4 18h16"/>
-                                    </svg>
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="hidden md:block md:ml-10 md:pr-4">
-                        <a href="/contact"
-                           class="font-medium text-gray-500 hover:text-gray-900 transition duration-150 ease-in-out">Contact</a>
-                        <a href="https://veganhacktivists.org"
-                           class="ml-8 font-medium text-gray-500 hover:text-gray-900 transition duration-150 ease-in-out">About</a>
-                        @auth
-                            <a href="{{ route('groups.index') }}"
-                            class="ml-8 font-medium text-pine-600 hover:text-pine-900 transition duration-150 ease-in-out">Home</a>
-                        @endauth
-                        @guest
-                            <a href="{{ route('login') }}"
-                                class="ml-8 font-medium text-pine-600 hover:text-pine-900 transition duration-150 ease-in-out">Log in</a>
-                            <a href="{{ route('register') }}"
+                            <div class="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
+                                <div class="flex items-center justify-between w-full md:w-auto">
+                                    <a href="{{ env('APP_URL') ?? 'https://mydailydozen.org' }}" aria-label="Home">
+                                        <img class="h-8 w-auto sm:h-10" src="{{ asset('img/mddlogo.png') }}" alt="Logo">
+                                    </a>
+                                    <div class="-mr-2 flex items-center md:hidden">
+                                <a href="/contact"
+                                class="font-medium text-gray-500 hover:text-gray-900 transition duration-150 ease-in-out">Contact</a>
+                                @guest
+								<a href="{{ route('login') }}"
+                                class="ml-8 font-medium text-pine-600 hover:text-pine-900 transition duration-150 ease-in-out">Log
+                                    in</a>
+                                <a href="{{ route('register') }}"
                                 class="ml-8 font-medium text-pine-600 hover:text-pine-900 transition duration-150 ease-in-out">Register</a>
-                        @endguest
-                    </div>
+								@endguest
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="hidden md:block md:ml-10 md:pr-4">
+                                <a href="/contact"
+                                class="font-medium text-gray-500 hover:text-gray-900 transition duration-150 ease-in-out">Contact</a>
+                                @guest
+								<a href="{{ route('login') }}"
+                                class="ml-8 font-medium text-pine-600 hover:text-pine-900 transition duration-150 ease-in-out">Log
+                                    in</a>
+                                <a href="{{ route('register') }}"
+                                class="ml-8 font-medium text-pine-600 hover:text-pine-900 transition duration-150 ease-in-out">Register</a>
+								@endguest
+                            </div>
                 </nav>
             </div>
 
