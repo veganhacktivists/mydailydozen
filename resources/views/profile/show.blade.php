b3bb0c883034653f7e9e435bd99f097bd5bf230c
David van Beveren <admin@veganhacktivists.org>
2020-11-25 22:09:45 +0800

    Remove browser session + 2 factor

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -17,20 +17,6 @@
                 </div>
             @endif
 
-            @if (Laravel\Fortify\Features::canManageTwoFactorAuthentication())
-                <x-jet-section-border />
-
-                <div class="mt-10 sm:mt-0">
-                    @livewire('profile.two-factor-authentication-form')
-                </div>
-            @endif
-
-            <x-jet-section-border />
-
-            <div class="mt-10 sm:mt-0">
-                @livewire('profile.logout-other-browser-sessions-form')
-            </div>
-
             <x-jet-section-border />
 
             <div class="mt-10 sm:mt-0">
