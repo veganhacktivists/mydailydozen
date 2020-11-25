e45f49963fd6345a92305af600e7870ce2742f86
David van Beveren <admin@veganhacktivists.org>
2020-11-25 13:58:01 +0800

    Update footer.blade.php

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -3,7 +3,7 @@
         <div class="max-w-screen-xl mx-auto py-4 px-4 sm:px-6 lg:px-8">
             <div class="flex flex-wrap items-center justify-between">
                  <p class="text-base leading-6 text-gray-400 md:mt-0 md:order-1">
-                    Built with love by the <a href="https://veganhacktivists.org" target="_blank"><u>Vegan Hacktivists</u></a> 💕 <u><a href="https://www.patreon.com/veganhacktivists" target="_blank" style="color:#609c2a;">Donate?</a>
+                    Built with love by the <a href="https://veganhacktivists.org" target="_blank"><u>Vegan Hacktivists</u></a> 💕 <a href="https://www.patreon.com/veganhacktivists" target="_blank" style="color:#609c2a;"><u>Donate?</u></a>
                 </p>
                 <p class="text-base leading-6 text-gray-400 md:mt-0 md:order-1">
                     <a href="https://play.google.com/store/apps/details?id=org.nutritionfacts.dailydozen&hl=en" target="_blank">Android</a>
