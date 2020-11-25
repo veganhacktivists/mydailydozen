3c3bbf25e64bb4873a963ae83e9981f0bd2c9c65
David van Beveren <admin@veganhacktivists.org>
2020-11-25 13:56:44 +0800

    Update footer.blade.php

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -3,12 +3,12 @@
         <div class="max-w-screen-xl mx-auto py-4 px-4 sm:px-6 lg:px-8">
             <div class="flex flex-wrap items-center justify-between">
                  <p class="text-base leading-6 text-gray-400 md:mt-0 md:order-1">
-                    Built with love by the Vegan Hacktivists 💕
+                    Built with love by the <a href="https://veganhacktivists.org" target="_blank"><u>Vegan Hacktivists</u></a> 💕 <u><a href="https://www.patreon.com/veganhacktivists" target="_blank" style="color:#609c2a;">Donate?</a>
                 </p>
                 <p class="text-base leading-6 text-gray-400 md:mt-0 md:order-1">
                     <a href="https://play.google.com/store/apps/details?id=org.nutritionfacts.dailydozen&hl=en" target="_blank">Android</a>
                     <a href="https://apps.apple.com/us/app/dr-gregers-daily-dozen/id1060700802" target="_blank">iOS</a>
-                    This app is <strong>not</strong> officially supported by <a href="https://nutritionfacts.org" target="_blank">Dr. Michael Greger</a>
+                    This app is <strong>not</strong> officially supported by <a href="https://nutritionfacts.org" target="_blank"><u>Dr. Michael Greger</u></a>.
                 </p>
             </div>
         </div>
