efe567486e19d5684110f403d1254da1124dbb76
David van Beveren <admin@veganhacktivists.org>
2020-11-25 17:44:54 +0800

    Update settings.blade.php

--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -1,24 +1,5 @@
 <x-master>
   <!-- Page header -->
-  <div class="bg-white shadow">
-    <div class="px-4 sm:px-6 lg:max-w-6xl lg:mx-auto lg:px-8">
-      <div class="py-6 md:flex md:items-center md:justify-between lg:border-t lg:border-cool-gray-200">
-        <div class="flex-1 min-w-0">
-          <!-- Profile -->
-          <div class="flex items-center">
-            <div>
-              <div class="flex items-center">
-                <h1 class="ml-3 text-2xl font-bold leading-7 text-cool-gray-900 sm:leading-9 sm:truncate">
-                  Enable or disable groups!
-                </h1>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-
   <div class="mt-8">
     <div class="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
       <div>
