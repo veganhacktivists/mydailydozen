51cc46f29481451234dacbf8a45be32d80eea9a8
Adrien <AdrienGiboire@users.noreply.github.com>
2020-11-28 12:27:33 +0100

    Display simple colored badges in history (#57)
    close https://trello.com/c/FvgBc6s2/84-history-use-amount-of-selected-groups

--- app/Http/Controllers/HistoryController.php
+++ app/Http/Controllers/HistoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Group;
+use App\Services\HistoryService;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
@@ -14,17 +15,11 @@ class HistoryController extends Controller
      * @return Application|Factory|View
      * @throws JsonException
      */
-  public function index()
+  public function index(HistoryService $historyService)
   {
-      $total = Group::all()->sum('per_day');
+      $user = auth()->user();
       return view('history')->with([
-          'history' => json_encode(auth()->user()->groups->pluck('pivot')->groupBy('recorded_at')->map(fn($item, $key) => [
-              'year' => substr($key, 0, 4),
-              'month' => substr($key, 5, 2),
-              'day' => substr($key, 8, 2),
-              'count' => $item->sum('checked'),
-              'percentage' => round($item->sum('checked') / $total * 100)
-          ])->values(), JSON_THROW_ON_ERROR),
+          'history' => json_encode($historyService->buildForUser($user)->values(), JSON_THROW_ON_ERROR),
           'totalPerDay' => Group::all()->sum('per_day')
       ]);
   }
