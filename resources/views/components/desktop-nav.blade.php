45080caaa40b3c3157e5870eb2692233edaba003
David van Beveren <admin@veganhacktivists.org>
2020-11-28 10:36:22 +0800

    Update desktop-nav.blade.php

--- resources/views/components/desktop-nav.blade.php
+++ resources/views/components/desktop-nav.blade.php
@@ -3,7 +3,7 @@
   <div class="flex flex-col w-64">
     <div class="flex flex-col flex-grow bg-pine-600 pt-5 pb-4 overflow-y-auto">
       <div class="bg-white rounded mx-5 flex items-center flex-shrink-0 px-4 py-4">
-        <img class="h-8 w-auto" src="{{ asset('img/mddlogo.png') }}" alt="My Daily Dozen" style="height: 40px;">
+        <a href="https://mydailydozen.org/groups"><img class="h-8 w-auto" src="{{ asset('img/mddlogo.png') }}" alt="My Daily Dozen" style="height: 40px;"></a>
       </div>
       <div class="mt-5 flex-1 flex flex-col overflow-y-auto">
         <div class="overflow-y-auto">
