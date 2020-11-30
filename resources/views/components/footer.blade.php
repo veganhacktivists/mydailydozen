55c2a8119084ca5d5d0d2f4a878e016f7e939576
unor <unor@gmx.com>
2020-11-30 06:40:26 +0100

    rel-noopener for _blank links

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -3,10 +3,10 @@
         <div class="max-w-screen-xl mx-auto py-4 px-4 sm:px-6 lg:px-8">
             <div class="flex flex-wrap items-center justify-between">
                  <p class="text-base leading-6 text-gray-400 md:mt-0 md:order-1">
-                    Built with love by the <a href="https://veganhacktivists.org" target="_blank"><u>Vegan Hacktivists</u></a> 💕 <a href="https://www.patreon.com/veganhacktivists" target="_blank" style="color:#609c2a;">Donate?</a>
+                    Built with love by the <a href="https://veganhacktivists.org" target="_blank" rel="noopener"><u>Vegan Hacktivists</u></a> 💕 <a href="https://www.patreon.com/veganhacktivists" target="_blank" rel="noopener" style="color:#609c2a;">Donate?</a>
                 </p>
                 <p class="text-base leading-6 text-gray-400 md:mt-0 md:order-1">
-                    This site is <strong>not</strong> officially associated by <a href="https://nutritionfacts.org" target="_blank"><u>Dr. Michael Greger</u></a>.
+                    This site is <strong>not</strong> officially associated by <a href="https://nutritionfacts.org" target="_blank" rel="noopener"><u>Dr. Michael Greger</u></a>.
                 </p>
             </div>
         </div>
