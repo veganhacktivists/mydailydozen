adcd3faceaa6f2210d40f108be40aa8eed447858
unor <6901294+unor@users.noreply.github.com>
2020-11-30 10:50:30 +0100

    logo alt; empty alt for banner (#60)
    Co-authored-by: unor <unor@gmx.com>

--- resources/views/contact-public.blade.php
+++ resources/views/contact-public.blade.php
@@ -40,7 +40,7 @@
                             <div class="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
                                 <div class="flex items-center justify-between w-full md:w-auto">
                                     <a href="{{ env('APP_URL') ?? 'https://mydailydozen.org' }}" aria-label="Home">
-                                        <img class="h-8 w-auto sm:h-10" src="{{ asset('img/mddlogo.png') }}" alt="Logo">
+                                        <img class="h-8 w-auto sm:h-10" src="{{ asset('img/mddlogo.png') }}" alt="Dr. Greger’s Daily Dozen">
                                     </a>
                                     <div class="-mr-2 flex items-center md:hidden">
                                 <a href="/contact"
