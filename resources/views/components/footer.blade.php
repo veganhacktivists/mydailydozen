93bcdfa70395f2ba9c5fe8bb3dd46fe9612793d0
David van Beveren <admin@veganhacktivists.org>
2020-12-01 11:46:46 +0800

    Update footer.blade.php

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -6,7 +6,7 @@
                     Built with love by the <a href="https://veganhacktivists.org" target="_blank" rel="noopener"><u>Vegan Hacktivists</u></a> 💕 <a href="https://www.patreon.com/veganhacktivists" target="_blank" rel="noopener" style="color:#609c2a;">Donate?</a>
                 </p>
                 <p class="text-base leading-6 text-gray-400 md:mt-0 md:order-1">
-                    This site is <strong>not</strong> officially associated by <a href="https://nutritionfacts.org" target="_blank" rel="noopener"><u>Dr. Michael Greger</u></a>.
+                    This site is <strong>not</strong> officially associated with <a href="https://nutritionfacts.org" target="_blank" rel="noopener"><u>Dr. Michael Greger</u></a>.
                 </p>
             </div>
         </div>
