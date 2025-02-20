8073e5a191a8e79d6bc29cb07da3caca782b9e74
Joaquín Triñanes <joaquin@veganhacktivists.org>
2025-02-20 13:50:24 +0100

    GDPR compliant fonts

--- resources/views/contact-public.blade.php
+++ resources/views/contact-public.blade.php
@@ -4,7 +4,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
     @vite(['resources/css/app.css'])
 	<title>Contact us – My Daily Dozen: track the foods recommended by NutritionFacts.org!</title>
 
