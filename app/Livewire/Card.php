70ad30606ebcc134eeb7ddad632c7aa74fbc13db
Joaquín Triñanes <joaquin@veganhacktivists.org>
2025-02-20 12:18:01 +0100

    Fix livewire checkboxes

--- app/Livewire/Card.php
+++ app/Livewire/Card.php
@@ -5,17 +5,27 @@ namespace App\Livewire;
 use Livewire\Component;
 use App\Models\Group;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Auth;
 
 class Card extends Component
 {
     public Group $group;
+
     public $checkCount;
+    public array $checkboxes = [];
+
+    private function updateCheckboxes()
+    {
+        $this->checkboxes = [...array_fill(0, $this->checkCount, true), ...array_fill(0, $this->group->per_day - $this->checkCount, false)];
+    }
 
     public function mount(Group $group)
     {
         $this->group = $group;
-        $this->checkCount = auth()->user()->getCheckCountForGroupAndDate($this->group, Carbon::today());
+        $this->checkCount = Auth::user()->getCheckCountForGroupAndDate($this->group, Carbon::today());
+        $this->updateCheckboxes();
     }
+
     public function render()
     {
         return view('livewire.card');
@@ -23,7 +33,8 @@ class Card extends Component
 
     public function check($count)
     {
-        $update = auth()->user()->setCheckCountForGroupAndDate($this->group, Carbon::today(), $count);
+        $update = Auth::user()->setCheckCountForGroupAndDate($this->group, Carbon::today(), $count);
         $this->checkCount = $update;
+        $this->updateCheckboxes();
     }
 }
