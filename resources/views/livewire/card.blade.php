ce48d0e477ed8498a497e1596247ff8a2bb1e4bd
Joaquín Triñanes <joaquin@veganhacktivists.org>
2025-03-11 10:01:04 +0100

    Fix card truncation overflow

--- resources/views/livewire/card.blade.php
+++ resources/views/livewire/card.blade.php
@@ -5,15 +5,15 @@
         <div class="flex-shrink-0 rounded-xl overflow-hidden">
             <img class="rounded-xl size-16" src="{{ $group->icon_location }}" alt="">
         </div>
-        <div class="flex justify-between flex-grow flex-col">
-            <div class="flex items-center justify-between">
-                <div class="flex items-center truncate">
-                    <div class="min-w-0 truncate">
-                        <h6 class="text-xl font-medium mr-3 truncate">
+        <div class="flex justify-between flex-grow min-w-0 flex-col">
+            <div class="flex gap-3 items-center justify-between">
+                <div class="flex flex-shrink gap-3 items-center min-w-0">
+                    <div class="min-w-0 w-fit max-w-full">
+                        <h6 class="text-xl font-medium truncate">
                             {{ $group['name'] }}
                         </h6>
                     </div>
-                    <div class="flex flex-shrink-0">
+                    <div class="flex-shrink-0">
                         <a href="/groups/{{ $group['id'] }}/">
                             <x-icons.information-circle class="max-w-full text-[#bababa]" />
                         </a>
@@ -30,10 +30,11 @@
                     {{ $group['subtitle'] }}
                 </div>
             @endif
-            <div class="flex-1 flex items-end justify-end">
+            <div class="flex items-end justify-end">
                 <div class="flex items-center">
-                    <span class="text-muted text-xs"
-                        style="font-size: 15px;font-weight: 500;color: #4e4e4e87;">{{ $checkCount ?? 0 }} /
+                    <span class="text-muted text-[15px] font-medium text-[#4e4e4e]/35"
+                    >
+                        {{ $checkCount ?? 0 }} /
                         {{ $group->per_day}}</span>
                     <div class="flex text-lg leading-7 font-medium text-cool-gray-900">
                         @for ($i = 0; $i < $group['per_day']; $i++)
