51786cc22e18a8fe59f011f1ca60586a5af3bb89
Joaquín Triñanes <joaquin@veganhacktivists.org>
2025-03-11 11:53:42 +0100

    Fix mobile footer styles

--- resources/views/components/link.blade.php
+++ resources/views/components/link.blade.php
@@ -1,3 +1,7 @@
-<a {{ $attributes->merge(['target' => 'blank', 'rel' => 'noopener']) }}>
+<a {{
+        $attributes->merge(['target' => 'blank', 'rel' => 'noopener'])
+        // these styles are not set globally because they'll break most existing links, which only have the default color set
+        ->class('text-pine-600 hover:text-pine-500 active:text-pine-700')
+    }}>
     {{ $slot }}
 </a>
