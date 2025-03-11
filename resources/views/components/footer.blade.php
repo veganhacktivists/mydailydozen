c1fba22f3f4f79aef873ef5e9af253badd94a676
Joaquín Triñanes <joaquin@veganhacktivists.org>
2025-03-11 11:45:47 +0100

    Change footer text

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,13 +1,18 @@
 <footer>
     <div class="bg-white">
         <div class="max-w-screen-xl mx-auto py-4 px-4 sm:px-6 lg:px-8">
-            <div class="flex flex-wrap items-center justify-between">
-                 <p class="text-base leading-6 text-gray-400 md:mt-0 md:order-1">
-                    Built with love by the <a href="https://veganhacktivists.org" target="_blank" rel="noopener"><u>Vegan Hacktivists</u></a> 💕 <a href="https://www.patreon.com/veganhacktivists" target="_blank" rel="noopener" style="color:#609c2a;">Donate?</a>
-                </p>
-                <p class="text-base leading-6 text-gray-400 md:mt-0 md:order-1">
-                    This site is <strong>not</strong> officially associated with <a href="https://nutritionfacts.org" target="_blank" rel="noopener"><u>Dr. Michael Greger</u></a>.
-                </p>
+            <div class="flex flex-wrap items-center text-gray-400 justify-between">
+                 <div class="text-base leading-6 md:mt-0 md:order-1">
+                    Get the <x-link href="https://apps.apple.com/us/app/dr-gregers-daily-dozen/id1060700802">iOS</x-link>
+                    or
+                    <x-link href="https://play.google.com/store/apps/details?id=org.nutritionfacts.dailydozen">Android app</x-link>
+                </div>
+                <div class="flex flex-row gap-4 text-base leading-6 md:mt-0 md:order-1">
+                    <div>Visit <x-link href="https://nutritionfacts.org/">NutritionFacts.org</x-link></div>
+                    <div>
+                        <x-link href="mailto:contact@mydailydozen.org">Get in touch!</x-link>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
