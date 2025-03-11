fc3507a91ca97462b6b2b8b988cb3e50f23b64fc
Joaquín Triñanes <joaquin@veganhacktivists.org>
2025-03-11 11:53:42 +0100

    Fix mobile footer styles

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -2,12 +2,12 @@
     <div class="bg-white">
         <div class="max-w-screen-xl mx-auto py-4 px-4 sm:px-6 lg:px-8">
             <div class="flex flex-wrap items-center text-gray-400 justify-between">
-                 <div class="text-base leading-6 md:mt-0 md:order-1">
+                 <div class="text-base leading-6 md:mt-0 md:order-1 mx-auto">
                     Get the <x-link href="https://apps.apple.com/us/app/dr-gregers-daily-dozen/id1060700802">iOS</x-link>
                     or
                     <x-link href="https://play.google.com/store/apps/details?id=org.nutritionfacts.dailydozen">Android app</x-link>
                 </div>
-                <div class="flex flex-row gap-4 text-base leading-6 md:mt-0 md:order-1">
+                <div class="place-items-center flex md:flex-row flex-col gap-x-4 text-base mx-auto leading-6 md:mt-0 md:order-1">
                     <div>Visit <x-link href="https://nutritionfacts.org/">NutritionFacts.org</x-link></div>
                     <div>
                         <x-link href="mailto:contact@mydailydozen.org">Get in touch!</x-link>
