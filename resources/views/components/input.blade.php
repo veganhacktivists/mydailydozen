288e02f7fa949bee2ecfffcf6ff15d4c4f366a18
Joaquín Triñanes <joaquin@veganhacktivists.org>
2025-03-12 11:11:16 +0100

    Fix input styles in contact forms

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -1,1 +1,1 @@
-<input {{ $attributes->class('border-gray-300 focus:border-blue-200 focus:ring-blue-200 rounded-md shadow-sm disabled:cursor-not-allowed disabled:bg-gray-200 p-1') }}>
+<input {{ $attributes->class('border-gray-300 border focus:border-blue-200 focus:ring-blue-200 rounded-md shadow-sm disabled:cursor-not-allowed disabled:bg-gray-200 p-1')->merge(['type' => 'text']) }} />
