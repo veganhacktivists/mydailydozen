152703e7dfaf30d33a56ad50b3a8279a84282913
Joaquín Triñanes <joaquin@veganhacktivists.org>
2025-03-13 21:13:52 +0100

    Disable insert_final_newline for blade files
    And remove newline from link file to prevent extra spaces

--- resources/views/components/link.blade.php
+++ resources/views/components/link.blade.php
@@ -2,6 +2,4 @@
         $attributes->merge(['target' => 'blank', 'rel' => 'noopener'])
         // these styles are not set globally because they'll break most existing links, which only have the default color set
         ->class('text-pine-600 hover:text-pine-500 active:text-pine-700')
-    }}>
-    {{ $slot }}
-</a>
+    }}>{{ $slot }}</a>
